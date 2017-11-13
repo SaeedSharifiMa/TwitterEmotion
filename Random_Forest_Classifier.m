95ccfa348a58263550199c233a5eb48978de2d28
Christopher Jung <christopherjung@huntsman-ve508-0529.apn.wlan.upenn.edu>
2017-11-13 14:44:16 -0500

    use only non-sparse features

--- Random_Forest_Classifier.m
+++ Random_Forest_Classifier.m
@@ -1,16 +1,17 @@
-classdef Naive_Bayes_Classifier < handle
+classdef Random_Forest_Classifier < handle
    properties
-      nb_model
+      rf_model
    end
    methods
       function train(obj, X_train_bag, train_raw, Y_train)
         costs = [0 3 1 2 3; 4 0 2 3 2; 1 2 0 2 1; 2 1 2 0 2; 2 2 2 1 0];
 
-         obj.nb_model = fitcnb(X_train_bag, Y_train, 'Distribution','mn','Cost', costs);
+         obj.rf_model = TreeBagger(50, full(X_train_bag), Y_train, 'Cost', costs);
       end
       
       function y_pred = predict(obj, X_test_bag, test_raw)
-          y_pred = predict(obj.nb_model, X_test_bag);
+          y_pred_str = predict(obj.rf_model, full(X_test_bag));
+          y_pred = str2num(cell2mat(y_pred_str));
       end
    end
 end
--- predict_labels.m
+++ predict_labels.m
@@ -5,7 +5,25 @@ function [Y_hat] = predict_labels(X_test_bag, test_raw)
 
 % Outputs:  Y_hat           nx1 predicted labels (1 for joy, 2 for sadness, 3 for surprise, 4 for anger, 5 for fear)
     load('train.mat');
-    nb_classifier = Naive_Bayes_Classifier;
-    nb_classifier.train(X_train_bag, train_raw, Y_train);
-    Y_hat = nb_classifier.predict(X_test_bag, test_raw);
+    [n_train,p] = size(X_train_bag);
+    n_test = size(X_test_bag, 1);
+    X_train_mean = mean(X_train_bag);
+    
+    not_sparse_features = X_train_mean > 10./n_train;
+    
+    X_train_mean_not_sparse = X_train_mean(not_sparse_features);
+    X_train_not_sparse = X_train_bag(:, not_sparse_features);
+    X_test_not_sparse = X_test_bag(:, not_sparse_features);
+    
+    [coeff, score, latenet] = pca(full(X_train_not_sparse));
+    
+    X_train_not_sparse_centered = X_train_not_sparse - repmat(X_train_mean_not_sparse, n_train, 1);
+    X_test_not_sparse_centered = X_test_not_sparse - repmat(X_train_mean_not_sparse, n_test, 1);
+    
+    X_train_not_sparse_pca = X_train_not_sparse_centered * coeff;
+    X_test_not_sparse_pca = X_test_not_sparse_centered * coeff;
+    rf_classifier = Random_Forest_Classifier;
+    
+    rf_classifier.train(X_train_not_sparse_pca, train_raw, Y_train);
+    Y_hat = rf_classifier.predict(X_test_not_sparse_pca, test_raw);
 end
